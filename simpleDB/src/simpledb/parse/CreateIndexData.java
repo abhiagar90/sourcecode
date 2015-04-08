999c7dc29d9b6f32de5d7e19fb43c9b640b90f2e
abhiagar90 <abhiagar90@gmail.com>
2015-04-09 01:39:14 +0530

    first push with basic parse running

---
+++ simpleDB/src/simpledb/parse/CreateIndexData.java
@@ -0,0 +1,54 @@
+package simpledb.parse;
+
+/**
+ * The parser for the <i>create index</i> statement.
+ * 
+ * @author Edward Sciore
+ */
+public class CreateIndexData {
+	private String idxname, tblname, fldname, indextype;
+
+	/**
+	 * Saves the table and field names of the specified index.
+	 */
+	public CreateIndexData(String idxname, String tblname, String fldname, String indextype) {
+		this.idxname = idxname;
+		this.tblname = tblname;
+		this.fldname = fldname;
+		this.indextype = indextype;
+		
+		//TODO: remove
+		System.out.println("inside the CreateIndexData constructor: " + indextype);
+	}
+
+	/**
+	 * Returns the name of the index.
+	 * 
+	 * @return the name of the index
+	 */
+	public String indexName() {
+		return idxname;
+	}
+
+	/**
+	 * Returns the name of the indexed table.
+	 * 
+	 * @return the name of the indexed table
+	 */
+	public String tableName() {
+		return tblname;
+	}
+
+	/**
+	 * Returns the name of the indexed field.
+	 * 
+	 * @return the name of the indexed field
+	 */
+	public String fieldName() {
+		return fldname;
+	}
+
+	public String indexType() {
+		return indextype;
+	}
+}
